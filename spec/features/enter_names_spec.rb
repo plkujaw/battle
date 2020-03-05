17eecdbca88357637c42281bafdac1203f28f33a
plkujaw <jawor.jakub@gmail.com>
2020-03-05 14:09:37 +0000

    DRYed up code with helpers

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature "Enter names" do
   scenario "Players submitting their names" do
-    visit("/")
-    fill_in :player_1_name, with: "Player 1"
-    fill_in :player_2_name, with: "Player 2"
-    click_button "LETS L0L FIGHT!"
+    sign_in_and_play
     expect(page).to have_content "Player 1 vs. Player 2 in EPIC L0L BATTLE!"
   end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature "Hit points" do
   scenario "Player 2 can see Player 1's HP" do
-    visit("/")
-    fill_in :player_1_name, with: "Player 1"
-    fill_in :player_2_name, with: "Player 2"
-    click_button "LETS L0L FIGHT!"
+    sign_in_and_play
     expect(page).to have_content "Player 2: 69HP"
   end
 end
