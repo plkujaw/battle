25f9e8cd7e46a0b36a7e9ce704ecd9296671d164
plkujaw <jawor.jakub@gmail.com>
2020-03-06 15:44:23 +0000

    skinny controllers

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,14 +1,27 @@
 require "game"
 
 describe Game do
-  subject(:game) { described_class.new }
-  let(:player1) { double :player }
-  let(:player2) { double :player }
+  subject(:game) { described_class.new(player_1, player_2) }
+  let(:player_1) { double :player }
+  let(:player_2) { double :player }
+
+  describe "#player_1" do
+    it "retrievs the first player" do
+      expect(game.player_1).to eq(player_1)
+    end
+  end
+
+  describe "#player_2" do
+    it "retrieves second playre" do
+      expect(game.player_2).to eq(player_2)
+    end
+  end
+
 
   describe "#attack" do
     it "damages the player" do
-      expect(player2).to receive(:receive_damage)
-      game.attack(player2)
+      expect(player_2).to receive(:receive_damage)
+      game.attack(player_2)
     end
   end
 end
